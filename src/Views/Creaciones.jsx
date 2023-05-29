0b244e6159d80d8289f4dc694ae1b5c9d5e21228
joak990 <joakhaidar@gmail.com>
2023-05-29 19:07:40 -0300

    views add

--- src/Views/Creaciones.jsx
+++ src/Views/Creaciones.jsx
@@ -1,11 +1,11 @@
 import React from 'react'
 
-function Home() {
+function Creaciones() {
   return (
     <div>
-    <h1>Soy el Home</h1>  
+      
     </div>
   )
 }
 
-export default Home
+export default Creaciones
