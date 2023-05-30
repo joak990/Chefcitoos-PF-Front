dcb59f77433a3d67b3c61741eaf0f2da780cfc24
joak990 <joakhaidar@gmail.com>
2023-05-29 23:21:50 -0300

    agregando about y su ruta

--- src/App.jsx
+++ src/App.jsx
@@ -2,12 +2,22 @@
 import { Route, Routes } from 'react-router-dom';
 import './App.css';
 import Home from './Views/Home';
+
+
+
+
+
 import About from './Views/About';
 function App() {
   return (
     <div >
         <Routes>
           <Route path="/home" element={<Home />}/>
+          
+
+
+
+          
           <Route path="/about" element={<About />}/>
         </Routes>
       
