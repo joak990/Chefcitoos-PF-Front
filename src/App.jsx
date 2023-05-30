f57d235416c8d17daf7fa48bb5e1156c50c829cc
Glendy Dugarte <glendydd@gmail.com>
2023-05-29 21:50:45 -0400

    notFound

--- src/App.jsx
+++ src/App.jsx
@@ -2,12 +2,14 @@
 import { Route, Routes } from 'react-router-dom';
 import './App.css';
 import Home from './Views/Home';
+import NotFound from './Views/NotFound';
 
 function App() {
   return (
     <div >
         <Routes>
           <Route path="/home" element={<Home />}/>
+          <Route path="*" element={<NotFound />}/>
         </Routes>
       
     </div>
