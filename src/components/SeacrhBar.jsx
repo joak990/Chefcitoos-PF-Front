3f324601c0fa7e5f474cbda6ecc50f06753ebc68
Glendy Dugarte <glendydd@gmail.com>
2023-05-30 22:17:46 -0400

    creaciones/navBar

--- src/components/SeacrhBar.jsx
+++ src/components/SeacrhBar.jsx
@@ -4,7 +4,7 @@ export default function SeacrhBar() {
   return (
     <div class="space-x-4">
       <input type="text" placeholder="Encuentra tu plato" class="border border-gray-300 px-4 py-2"></input>
-      <button type="submit" className="bg-orange-600 w-16 h-6 text-white rounded-md font-bold self-end mt-4">Buscar</button>
+      <button type="submit" className="bg-orange-600 w-20 h-10 text-white rounded-xl font-bold mt-6">Buscar</button>
     </div>
   )
 }
