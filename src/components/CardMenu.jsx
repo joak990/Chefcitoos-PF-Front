908aed000b7e637c3af52302b493ca04a1b115c9
joak990 <joakhaidar@gmail.com>
2023-05-31 18:15:49 -0300

    menu con cards add

---
+++ src/components/CardMenu.jsx
@@ -0,0 +1,42 @@
+import React, { useState } from "react";
+import burguer from "../img/hamburguesa.jpg";
+
+function CardMenu({ id, image, name, description, price }) {
+  const [isHovered, setIsHovered] = useState(false);
+
+  const handleMouseEnter = () => {
+    setIsHovered(true);
+  };
+
+  const handleMouseLeave = () => {
+    setIsHovered(false);
+  };
+
+  const cardStyle = {
+    width: isHovered ? "330px" : "296px",
+   
+    transition: "width 0.4s ease, height 0.5s ease",
+    borderBottom: "1px solid #00000015",
+  };
+
+  return (
+    <div
+      className="flex flex-row items-center  justify-center p-8  "
+      style={cardStyle}
+      onMouseEnter={handleMouseEnter}
+      onMouseLeave={handleMouseLeave}
+    >
+      <div className="flex flex-col">
+        <h6 className="font-semibold text-lg">Hamburguesa</h6>
+        <p className="text-gray-800"> Pepinillo, tomate, lechuga, mostaza....</p>
+        <p className="font-semibold text-orange-600 text-lg mt-2">$28000</p>
+        <p className="text-sm"> </p>
+      </div>
+      <div className="flex flex-grow justify-end">
+        <img className="w-32 rounded-lg" src={burguer} alt="" />
+      </div>
+    </div>
+  );
+}
+
+export default CardMenu;
