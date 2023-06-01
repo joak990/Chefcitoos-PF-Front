fffba3b04e7711c8db0fe0ff8eda49d2dd602465
Greidy Peña <greidykp@gmail.com>
2023-06-01 16:26:23 -0500

    Productos renderizados en menu

--- src/components/CardMenu.jsx
+++ src/components/CardMenu.jsx
@@ -1,7 +1,7 @@
 import React, { useState } from "react";
 import burguer from "../img/hamburguesa.jpg";
 
-function CardMenu({ id, image, name, description, price, onOrderProduct }) {
+function CardMenu({ product, onOrderProduct }) {
   const [isHovered, setIsHovered] = useState(false);
 
   const handleMouseEnter = () => {
@@ -13,7 +13,7 @@ function CardMenu({ id, image, name, description, price, onOrderProduct }) {
   };
 
   const cardStyle = {
-    width: isHovered ? "330px" : "296px",
+    width: isHovered ? "310px" : "296px",
    
     transition: "width 0.4s ease, height 0.5s ease",
     borderBottom: "1px solid #00000015",
@@ -21,20 +21,19 @@ function CardMenu({ id, image, name, description, price, onOrderProduct }) {
 
   return (
     <div
-      className="flex flex-row items-center  justify-center p-8 "
+      className="flex flex-row justify-center py-1 my-2"
       style={cardStyle}
       onMouseEnter={handleMouseEnter}
       onMouseLeave={handleMouseLeave}
       onClick={onOrderProduct}
     >
-      <div className="flex flex-col">
-        <h6 className="font-semibold text-lg">Hamburguesa</h6>
-        <p className="text-gray-800"> Pepinillo, tomate, lechuga, mostaza....</p>
-        <p className="font-semibold text-orange-600 text-lg mt-2">$28000</p>
-        <p className="text-sm"> </p>
+      <div className="flex flex-col items-start self-start">
+        <h6 className="font-semibold text-md uppercase">{product?.name}</h6>
+        <p className="text-gray-700 w-52 text-sm mr-1">{product?.elements}</p>
+        <p className="font-semibold text-orange-600 text-lg mt-1">${product?.price}</p>
       </div>
       <div className="flex flex-grow justify-end">
-        <img className="w-32 rounded-lg" src={burguer} alt="" />
+        <img className="rounded-lg object-cover" src={product?.image} alt="" />
       </div>
     </div>
   );
