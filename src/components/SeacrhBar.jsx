1fd0a330719b0f30f1360fb3c9c3cecbfd82934c
Glendy Dugarte <glendydd@gmail.com>
2023-06-04 20:46:59 -0400

    readysb

--- src/components/SeacrhBar.jsx
+++ src/components/SeacrhBar.jsx
@@ -20,14 +20,25 @@ function handleSearch(){
 
  }
 }
-
+const handleKeyUp = (event) => {
+  if ( event.keyCode === 13 || event.charCode === 13 ) {
+    if (type == 'creations') {
+      dispatch(getCreationByName(userId, name))
+    }
+   else {
+    dispatch(getPublicationName(name))
+  
+   }
+   
+  }
+};
 
 
 
 
   return (
     <div class="space-x-4">
-      <input type="text" placeholder="Encuentra tu plato" class="border border-gray-300 px-4 py-2" onChange={(e)=>handleInputChange(e)}></input>
+      <input  type="text" placeholder="Encuentra tu plato" class="border border-gray-300 px-4 py-2"  onKeyUp={handleKeyUp}onChange={(e)=>handleInputChange(e)}></input>
       <button type="submit" className="bg-orange-600 w-20 h-10 text-white rounded-xl font-bold mt-6" onClick = {handleSearch}>Buscar</button>
     </div>
   )
