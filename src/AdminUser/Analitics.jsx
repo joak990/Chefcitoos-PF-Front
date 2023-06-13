f78de84048049a0738005c4d5015388e818706ca
joak990 <joakhaidar@gmail.com>
2023-06-13 09:35:39 -0300

    analitics

---
+++ src/AdminUser/Analitics.jsx
@@ -0,0 +1,11 @@
+import { Analytics } from '@vercel/analytics/react';
+ 
+export default function App() {
+  return (
+    <html lang="en">
+      <body>
+        <Analytics />
+      </body>
+    </html>
+  );
+}
