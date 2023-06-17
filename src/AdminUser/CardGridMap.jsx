8f288d3d885cc2b3e7cf5f9136364e4b55f71eba
joak990 <joakhaidar@gmail.com>
2023-06-17 13:04:50 -0300

    addbugs

--- src/AdminUser/CardGridMap.jsx
+++ src/AdminUser/CardGridMap.jsx
@@ -1,59 +1,71 @@
 import React, { useEffect } from 'react';
 import { BadgeDelta, Flex, Metric, Text } from '@tremor/react';
 import { Card } from '@tremor/react';
+import { useSelector, useDispatch } from 'react-redux';
 import { getsalesandpercentaje } from '../Redux/actions';
-import { useDispatch, useSelector } from 'react-redux';
 
 function CardGridMap() {
-  const data = [
-    {
-      title: 'Ventas',
-      metric: '$12,100',
-      progress: 15.9,
-      deltatype: 'increase',
-    },
-    {
-      title: 'Ganancia',
-      metric: '$12,400',
-      progress: 15.9,
-      deltatype: 'increase',
-    },
-    {
-      title: 'Clientes',
-     
-      progress: 15.9,
-      deltatype: 'increase',
-    },
-  ];
-const dispatch = useDispatch()
-  useEffect(() => {
-  
-    dispatch(getsalesandpercentaje());
-  }, []);
   const salesandpercentaje = useSelector((state) => state.salesandpercentaje);
+  const dispatch = useDispatch();
+
+  useEffect(() => {
+    dispatch(getsalesandpercentaje())
+      .then(response => {
+   
+        dispatch({ type: 'SET_SALES_AND_PERCENTAJE', payload: response });
+      })
+      .catch(error => {
+      
+      });
+  }, [dispatch]);
+
+  useEffect(() => {
+    const savedState = localStorage.getItem('salesandpercentaje');
+    if (savedState) {
+      dispatch({ type: 'SET_SALES_AND_PERCENTAJE', payload: JSON.parse(savedState) });
+    }
+  }, [dispatch]);
+
+  useEffect(() => {
+    const saveStateToLocalStorage = () => {
+      localStorage.setItem('salesandpercentaje', JSON.stringify(salesandpercentaje));
+    };
+
+    window.addEventListener('beforeunload', saveStateToLocalStorage);
+    return () => {
+      window.removeEventListener('beforeunload', saveStateToLocalStorage);
+    };
+  }, [salesandpercentaje]);
 
   return (
     <div className='flex flex-wrap justify-center gap-4 mx-auto'>
-     
-     <Card className="max-w-sm">
-    <Flex justifyContent="between" alignItems="center">
-      <Text>Ventas</Text>
-      <BadgeDelta  deltaType={salesandpercentaje?.ventas?.porcentaje < 0 ? 'decrease' : 'increase'} isIncreasePositive={true} size="xs">
-        {salesandpercentaje?.ventas?.porcentaje}%
-      </BadgeDelta>
-    </Flex>
-    <Metric>${salesandpercentaje?.ventas?.total}</Metric>
-  </Card>
+      <Card className="max-w-sm">
+        <Flex justifyContent="between" alignItems="center">
+          <Text>Ventas</Text>
+          <BadgeDelta
+            deltaType={salesandpercentaje?.ventas?.porcentaje < 0 ? 'decrease' : 'increase'}
+            isIncreasePositive={true}
+            size="xs"
+          >
+            {salesandpercentaje?.ventas?.porcentaje}%
+          </BadgeDelta>
+        </Flex>
+        <Metric>${salesandpercentaje?.ventas?.total}</Metric>
+      </Card>
     
-  <Card className="max-w-sm">
-    <Flex justifyContent="between" alignItems="center">
-      <Text>Clientes nuevos</Text>
-      <BadgeDelta  deltaType={salesandpercentaje?.clientes.porcentaje < 0 ? 'decrease' : 'increase'} isIncreasePositive={true} size="xs">
-        {salesandpercentaje?.clientes?.porcentaje}%
-      </BadgeDelta>
-    </Flex>
-    <Metric>{salesandpercentaje?.clientes?.total}</Metric>
-  </Card>
+      <Card className="max-w-sm">
+        <Flex justifyContent="between" alignItems="center">
+          <Text>Clientes nuevos</Text>
+          <BadgeDelta
+            deltaType={salesandpercentaje?.clientes?.porcentaje < 0 ? 'decrease' : 'increase'}
+            isIncreasePositive={true}
+            size="xs"
+          >
+            {salesandpercentaje?.clientes?.porcentaje}%
+          </BadgeDelta>
+        </Flex>
+        <Metric>{salesandpercentaje?.clientes?.total}</Metric>
+      </Card>
     </div>
   );
 }
